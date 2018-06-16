09caaac206f657245508c7944bcbeaa8a1b76339
KlebGomes <kleberson.magalhaes@gmail.com>
2018-06-16 11:39:52 -0300

    Bot main code

--- forobot.py
+++ forobot.py
@@ -10,6 +10,7 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
+from random import randint
 import time
 
 Client = discord.Client()
@@ -18,7 +19,7 @@ client = commands.Bot(command_prefix = "")
 @client.event
 async def on_ready():
     print('Bot is online and ready!')
-    await client.change_presence(game=discord.Game(name="For help say /help"))
+    await client.change_presence(game=discord.Game(name="To know the commands, just say /help"))
 
 @client.event
 async def on_message(message):
@@ -32,6 +33,30 @@ async def on_message(message):
         #args [2] = There
         #args [1:] = Hey There
         await client.send_message(message.channel, "%s" % (' '.join(args[1:])))
+
+    # This commands make the bot flip a coin
+    if message.content.lower().startswith('/flip'):
+        choice = randint(1, 2)
+        if choice == 1:
+            await client.add_reaction(message, '🌑')
+        if choice == 2:
+            await client.add_reaction(message, '🌕')
+
+    # Author's and bot information!
+    if message.content.lower().startswith("/info"):
+        info = discord.Embed(
+            title="I am IRO... I mean, Foro Bot!",
+            color=0xe74c3c,
+            description="Obrigado por usar o Foro Bot/Thank you for use Foro Bot:\n"
+                        "If you have any suggestions or found bugs in this bot, contact me at:\n"
+                        "DiscordID: Foromir#5783\n"
+                        "Twitter: _kleb"
+                        "\n"
+                        "\n"
+                        "Version: 0.1a"
+
+        )
+
 
 client.run("NDU3NTA4NDI5MDYyMzQwNjA4.DgaV2g.ZH5iQ1JYY4Sx89eZl00NQORmOro")
 
