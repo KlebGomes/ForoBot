cd127d138bcd8c6da8dbf41ed0a9ca0669ead82b
KlebGomes <kleberson.magalhaes@gmail.com>
2018-06-16 16:15:51 -0300

    Update forobot.py

--- forobot.py
+++ forobot.py
@@ -3,30 +3,35 @@ Name: ForoBot
 Description: This Discord bot is used to help admin with commands and bring fun to the server
 Author: Kleberson "Foromir" Gomes
 Author's contact: Twitter.com/_kleb
-Version: 0.4a
+Version: 0.3a
 '''
 
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
+import config
 import asyncio
 from random import randint
 import os
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = ';')
+client = commands.Bot(command_prefix=';')
+
 
 @client.event
 async def on_ready():
     print('Bot is online and ready!')
-    await client.change_presence(game=discord.Game(name="Commands? Just say ;help"))
+    await client.change_presence(game=discord.Game(name='Commands? Just say ;help'))
+
 
 @client.event
 async def on_message(message):
+    # Ping pong the bot
     if message.content.lower().startswith(';ping'):
         userID = message.author.id
-        await client.send_message(message.channel, ':ping_pong: **Pong!**')
+        await client.send_message(message.channel, '<%s>:ping_pong: **Pong!**' % userID)
 
+    # This command make the bot says anything
     if message.content.lower().startswith(';SAY'):
         args = message.content.split(' ')
         await client.send_message(message.channel, "%s" % (' '.join(args[1:])))
@@ -50,23 +55,24 @@ async def on_message(message):
                         "Twitter: _kleb"
                         "\n"
                         "\n"
-                        "Version: 0.4a"
+                        "Version: 0.3a"
 
         )
 
         await client.send_message(message.channel, embed=info)
 
-    # This command shows help command
+    # This command shows help
     if message.content.lower().startswith(';help'):
         help = discord.Embed(
             title='Commands and help bellow',
             color=0x751be2,
             description='Here is a list of all Foro\'s commands\n'
-            'flip, ping, help, info, roll, say'
+                        'flip, ping, help, info, roll, say'
 
         )
 
         await client.send_message(message.channel, embed=help)
 
+
 # Bot initialization on Heroku
-Client.run(str(os.environ.get('BOT_TOKEN')))
+Client.run(str(os.environ.get(config.BOT_TOKEN)))
